46c0b2f19f491f80e30a67c1bcead6cc9eef52ce
alimbetov <51784151+alimbetov@users.noreply.github.com>
2026-07-21 10:18:57 +0500

    Add Spring Testing lab order line entity

---
+++ 50_LABS/Spring/TEST-B01/src/main/java/lab/PurchaseOrderLine.java
@@ -0,0 +1,58 @@
+package lab;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "purchase_order_lines")
+public class PurchaseOrderLine {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String sku;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "order_id", nullable = false)
+    private PurchaseOrder order;
+
+    protected PurchaseOrderLine() {
+    }
+
+    PurchaseOrderLine(String sku, int quantity) {
+        this.sku = sku;
+        this.quantity = quantity;
+    }
+
+    void attachTo(PurchaseOrder order) {
+        this.order = order;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public PurchaseOrder getOrder() {
+        return order;
+    }
+}
