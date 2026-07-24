831b5b41cc9b4aab4680d75c633646b5dfea645f
alimbetov <51784151+alimbetov@users.noreply.github.com>
2026-07-24 07:15:31 +0500

    lab: add catalog item dto

---
+++ 50_LABS/Spring/SPRING-MVC-B02/src/main/java/lab/mvc/rest/CatalogItemDto.java
@@ -0,0 +1,41 @@
+package lab.mvc.rest;
+
+public class CatalogItemDto {
+
+    private Long id;
+    private String name;
+    private Integer quantity;
+
+    public CatalogItemDto() {
+    }
+
+    public CatalogItemDto(Long id, String name, Integer quantity) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
