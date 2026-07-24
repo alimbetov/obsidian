31ebabe2e7f2f0fd9f6fd9bd47bba5e156ead81a
alimbetov <51784151+alimbetov@users.noreply.github.com>
2026-07-24 07:15:09 +0500

    lab: add REST lab application

---
+++ 50_LABS/Spring/SPRING-MVC-B02/src/main/java/lab/mvc/rest/RestLabApplication.java
@@ -0,0 +1,12 @@
+package lab.mvc.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestLabApplication.class, args);
+    }
+}
